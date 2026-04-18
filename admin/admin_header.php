2000a0d656aae3cc76ef571caf38ccdd059db1f3
p4-49m <sengxiangt2@gmail.com>
2026-04-18 14:42:32 +0800

    header

--- admin/admin_header.php
+++ admin/admin_header.php
@@ -40,7 +40,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
                     <li><a href="manage_product.php" class="<?php echo $current_page == 'manage_product.php' ? 'active' : ''; ?>">📦 Manage Products</a></li>
                     <li><a href="add_product.php" class="<?php echo $current_page == 'add_product.php' ? 'active' : ''; ?>">➕ Add New Product</a></li>
                     <li><a href="category_maintenance.php" class="<?php echo $current_page == 'category_maintenance.php' ? 'active' : ''; ?>">📂 Category Maintenance</a></li>
-                    <li><a href="manage_orders.php" class="<?php echo $current_page == 'manage_orders.php' ? 'active' : ''; ?>">🛒 Orders</a></li>
+                    <li><a href="manage_orders.php" class="<?php echo $current_page == 'manage_orders.php' ? 'active' : ''; ?>">🛒 Manage Orders</a></li>
                 </ul>
             </aside>
             <main class="admin-main">
